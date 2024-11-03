ebf3e103a3e8aad9b9abd744b48313632729a022
Arya Segara <aryasegara10@gmail.com>
2024-11-04 06:09:41 +0700

    proses-supplier

--- src/main/java/com/tugas5/apotek/service/SupplierService.java
+++ src/main/java/com/tugas5/apotek/service/SupplierService.java
@@ -1,5 +1,17 @@
 package com.tugas5.apotek.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tugas5.apotek.model.Supplier;
+import com.tugas5.apotek.repository.SupplierRepository;
+
+@Service
 public class SupplierService {
-    
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public void save(Supplier supplier){
+        supplierRepository.save(supplier);
+    }
 }
